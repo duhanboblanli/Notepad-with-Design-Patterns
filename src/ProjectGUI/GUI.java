3b2e982b9fbbfe231ce3dc671bf2de15330e85a2
AdemSoydan <ademm.soydan@gmail.com>
2022-12-29 23:34:53 +0300

    create first JFrame  window

--- src/ProjectGUI/GUI.java
+++ src/ProjectGUI/GUI.java
@@ -1,10 +1,30 @@
 package ProjectGUI;
 
+import javax.swing.JFrame;
+
 public class GUI {
+	
+	// jFrame icerisine buton vs koymamıza yarıyo
+	JFrame window;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new GUI();
+		
+		
+	}
+	
+	public GUI() {
+		createWindow();
+		window.setVisible(true);
+	}
+	
+	public void createWindow() {
+		
+		window = new JFrame("notepad");
+		window.setSize(800,600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 }
